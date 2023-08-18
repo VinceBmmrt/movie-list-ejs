a816bf0c02546c03483815a564fe4bbe9c7f9d70
Vince Bmmrt <vincebommert75@gmail.com>
2023-08-18 14:21:47 +0200

    movielist with search

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 3008;
+
+// Initialize EJS with Express
+app.set("view engine", "ejs");
+
+// Specify the directory for your views
+app.set("views", "views");
+
+// Middleware to serve static files from the "public" directory
+app.use(express.static("public"));
+
+// Import the JSON file
+const movies = require("./movies.json");
+
+app.get("/", (req, res) => {
+  // Render the "index" file, passing the "movies" data
+  res.render("index", { movies });
+});
+
+// Dynamic route to retrieve movie information based on the movieName parameter
+app.get("/movie/:movieName", (req, res) => {
+  // Extract the movieName parameter from the URL
+  const movieUrl = req.params.movieName;
+
+  // Find the movie in the "movies" array using the URL
+  const goodMovie = movies.find((movie) => movie.url === movieUrl);
+
+  // Render the "movie" file, passing the movie object
+  res.render("movie", { movie: goodMovie });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
